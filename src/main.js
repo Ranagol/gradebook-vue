946006cc3b532d6d868174a9dda97aecd367c434
Andor <andorhorvat@gmail.com>
2020-04-12 10:09:28 +0200

    Axios done

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import { store } from './store/store';
 
 Vue.use(VueRouter);
 const router = new VueRouter({
@@ -13,6 +14,7 @@ const router = new VueRouter({
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
