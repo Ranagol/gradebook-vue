2556fe50cc0d89bd9491d3f9b9dab6e50243bf84
Andor <andorhorvat@gmail.com>
2020-04-16 12:52:53 +0200

    /gradebooks conditional displaying sorted

--- src/components/gradebooks/CardGradebook.vue
+++ src/components/gradebooks/CardGradebook.vue
@@ -2,15 +2,22 @@
   <div class="card card-width border border-success ">
     <div class="card-header bg-success"></div>
     <div class="card-body">
-      <h5 class="card-title">
+      <h4 class="card-title">
         <router-link :to="`/gradebooks/${gradebook.id}`" class="link">
           {{ gradebook.name }}
         </router-link>
-      </h5>
-      <p class="card-text">Profesor: </p>
-      <router-link :to="`/professors/${professor.id}`" class="link">
-        {{ professor.first_name }} {{ professor.last_name }}
-      </router-link>
+      </h4>
+
+      <!--Professor conditional displaying  -->
+      <div v-if="gradebook.professor !== undefined && gradebook.professor !==null">
+        <p class="card-text">Profesor: </p>
+        <router-link :to="`/professors/${professor.id}`" class="link">
+          {{ professor.first_name }} {{ professor.last_name }}
+        </router-link>
+      </div>
+      <div v-else>
+        <p>This gradebook does not have a professor.</p>
+      </div>
       <p class="card-text">Vreme kreiranja:{{ gradebook.created_at}} </p>
     </div>
   </div>
